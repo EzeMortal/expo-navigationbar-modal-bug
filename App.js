21ee4f67afeb32a80da7fc3d8ee54d1e87c8a28b
Ezequiel <eze.1008@hotmail.com>
2020-05-14 21:12:03 -0300

    commit

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import Modal from 'react-native-modal';
 
 export default function App() {
+  const [visible, setVisible] = useState(false);
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+     <View style={styles.container}>
+       <TouchableOpacity onPress={() => setVisible(true)}>
+         <Text>Open modal</Text>
+       </TouchableOpacity>
+
+       <Modal
+          isVisible={visible}
+          onBackdropPress={handleClose}
+          onBackButtonPress={handleClose}
+          swipeDirection={'down'}
+          onSwipeComplete={handleClose}
+          backdropTransitionOutTiming={0}
+          backdropOpacity={0.5}
+          style={styles.modal}>
+         <View style={styles.modalContent}>
+           <Text>I am the modal content!</Text>
+         </View>
+       </Modal>
+     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  modal: {
+    margin: 0,
+    justifyContent: 'flex-end',
+  },
+  modalContent: {
+    height: 500,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 20,
   },
 });
